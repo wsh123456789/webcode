30aa994f12ab69fb6525bccc123ed384ec8978d6
wsh123456789 <88263983+wsh123456789@users.noreply.github.com>
2022-07-30 16:12:35 +0800

    base

--- JS/Ajax/原生Ajax/server.js
+++ JS/Ajax/原生Ajax/server.js
@@ -4,8 +4,7 @@ const express = require('express');
 //创建应用对象
 const app = express();
 
-//创建路由规则
-app.get('/server', (request, response)=>{
+app.all('/server', (request, response)=>{
     //设置响应头
     response.setHeader('Access-Control-Allow-Origin','*');
     response.setHeader('Access-Control-Allow-Header','*');
@@ -20,6 +19,36 @@ app.get('/server', (request, response)=>{
     },3000)
     //设置相应体
     // response.send(str);
+})
+//创建路由规则
+app.all('/fetch-server', (request, response)=>{
+    //设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Access-Control-Allow-Headers','*');
+    
+    const data = {
+        name: 'hahahaha'
+    };   
+    let str = JSON.stringify(data);
+    //延时相应
+    // setTimeout(()=>{
+    //     response.send('延时相应');
+    // },3000)
+    //设置相应体
+    response.send(str);
+})
+
+app.all('/jsonp-server', (request, response)=>{
+    //设置响应头
+    // response.setHeader('Access-Control-Allow-Origin','*');
+    // response.setHeader('Access-Control-Allow-Headers','*');
+    
+    const data = {
+        exist: 1,
+        msg: 'hahahaha'
+    };   
+    let str = JSON.stringify(data);
+    response.send(`handle(${str})`);
 })
 
 //监听端口
